dbaf959cf686b663c23d3fb497dc479bd4568aaa
Thang Duong <thang.duong@epfl.ch>
2021-03-19 09:09:55 +0000

    Add more labels for xlni

--- xlni_tech_classifier.py
+++ xlni_tech_classifier.py
@@ -6,7 +6,7 @@ classifier = pipeline("zero-shot-classification",
                       model="joeddav/xlm-roberta-large-xnli", device=0)
 # print(next(classifier.parameters()).is_cuda)
 
-candidate_labels = ["technology", "not about technology"]
+candidate_labels = ["quantity", "person", "organization", "brand", "facility", "location", "product", "event", "work" "of" "art", "technology", "animal"]
 
 tech_paths = ["wiki_abstracts/abtract_tech.json", "wiki_abstracts/empty_tech_intro.json"]
 non_tech_paths = ["wiki_abstracts/abtract_non_tech.json", "wiki_abstracts/empty_non_tech_intro.json"]
@@ -18,14 +18,15 @@ for path in tech_paths:
     for term in tqdm(js):
         abstract = js[term]
         if abstract != '':
+            labels = classifier(js[term], candidate_labels)['labels']
             scores = classifier(js[term], candidate_labels)['scores']
-            if scores[0]  >= scores[1]:
+            if labels[0] == 'technology' or labels[1] == 'technology':
                 tech_predicts.append(1)
             else:
                 tech_predicts.append(0)
         else:
             tech_predicts.append(-1)
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 
 non_tech_predicts = []            
 for path in non_tech_paths:
@@ -33,8 +34,9 @@ for path in non_tech_paths:
     for term in tqdm(js):
         abstract = js[term]
         if abstract != '':
+            labels = classifier(js[term], candidate_labels)['labels']
             scores = classifier(js[term], candidate_labels)['scores']
-            if scores[0]  >= scores[1]:
+            if labels[0] == 'technology' or labels[1] == 'technology':
                 non_tech_predicts.append(1)
             else:
                 non_tech_predicts.append(0)
